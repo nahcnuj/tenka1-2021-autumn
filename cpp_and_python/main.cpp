c80784ccd62d2d378eec959c2157120edf070095
Junichi Hayashi <junchan.kcct@gmail.com>
2021-09-23 16:58:45 +0900

    Revert "increase drop rate"
    This reverts commit d6ffd44ccdefe33ecd6c9fd017c4fe5d8fd9c9d4.

--- cpp_and_python/main.cpp
+++ cpp_and_python/main.cpp
@@ -183,9 +183,9 @@ struct Bot {
 			}
 		}
 		{
-			if (uniform_int_distribution<>(0, 100)(mt) < 30) {
+			if (uniform_int_distribution<>(0, 100)(mt) < 10) {
 				queue.pop();
-				if (uniform_int_distribution<>(0, 100)(mt) < 30) {
+				if (uniform_int_distribution<>(0, 100)(mt) < 10) {
 					queue.pop();
 				}
 			}
